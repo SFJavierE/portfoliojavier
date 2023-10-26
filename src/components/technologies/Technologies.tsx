51ad4fb91e8d5dc54eefa509ca40cd7102a44b2f
Zoaxx1 <sosafuch@gmail.com>
2023-10-26 16:54:23 -0300

    add technologies

--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -1,7 +1,32 @@
 export default function Technologies () {
+    const tFrontend : string[] = ['Javascript', 'React', 'Typescript', 'Angular', 'AngularJS', 'HTML', 'CSS', 'Sass', 'TailwindCss', 'Bootstrap'];
+    const tBackend: string[] = ['NodeJs', 'Express', 'Ruby on Rails', '.NET', 'C#', 'Postman'];
+    const tDataBase: string[] = ['SQL', 'GraphQL', 'Hasura', 'Postgress', 'Firebase'];
+    const tOthers: string[] = ['Confluence', 'Figma', 'Jira', 'Trello'];
+
+    const technologiesList = (list: string[]) => (
+        <div className="d-flex mt-3">
+            {
+                list.map((t) => technology(t))
+            }
+        </div>
+    )
+
+    const technology = (t: string) => (
+        <div className="bg-primary p-2 mx-2">
+            {t}
+        </div>
+    )  
+
     return(
-        <div>
-            Technologies
+        <div className="mb-4">
+            <span className="fs-4">
+                Technologies
+            </span>
+            { technologiesList(tFrontend) }
+            { technologiesList(tBackend) }
+            { technologiesList(tDataBase) }
+            { technologiesList(tOthers) }
         </div>
     )
 }
