b22bc322f01bb712be68b3a615f51d49f8f3cff2
Zoaxx1 <sosafuch@gmail.com>
2023-11-01 13:03:04 -0300

    add scroll

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -3,11 +3,6 @@ import Text from './text_es.json';
 export default function Welcome () {
     return (
         <div className="fs-5">
-            <div className='mb-4 bg-secondary sticky-top opacity-50 p-2 d-flex align-items-center justify-content-center' style={{width: '100%'}}>
-                <span className='text-warning'>
-                    EN PROGRESO: todavía no está completo el portafolio pero la información esta lista para revisarse.
-                </span>
-            </div>
             <span>
                 {Text._1}     
             </span>
@@ -19,7 +14,6 @@ export default function Welcome () {
             <span>
                 {Text._3}
             </span>
-            <br className='mb-3'/>
         </div>
     )
 }
